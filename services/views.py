c0c4bac3f6f2989d95a69986bbaed6faed01c879
Ático <atico@202CS1E01164403.local>
2018-04-28 13:06:09 -0500

    Creating footer

--- services/views.py
+++ services/views.py
@@ -3,6 +3,4 @@ from django.http  import HttpResponse
 
 
 def index(request):
-    print("hola soy un log de servidor.")
-    print("--entrando al index")
-    return HttpResponse("hola soy un index")
+    return render(request, "index.html")
